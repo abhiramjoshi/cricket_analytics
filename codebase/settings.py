6bc87007a01c77c8d927df3d7b020b0ae3815b38
Abhi_Joshi <joshi.abhiram98@gmail.com>
2022-08-15 19:10:06 +0100

    batting_analysis & aggregate_batting_analysis dev

--- codebase/settings.py
+++ codebase/settings.py
@@ -9,6 +9,7 @@ ANALYSIS = os.path.join(BASE_PATH, "data", 'analysis_data')
 SCORECARDS = os.path.join(BASE_PATH, "data", "scorecards")
 CAREERS = os.path.join(BASE_PATH, "data", "careers")
 LABEL_DATA = os.path.join(BASE_PATH, "data", "labelled_data")
+
 paths = [
     DATA_LOCATION,
     FULL_COMMS,
